ea8fc8f27d3332e57ff715f82359294228cbf8db
cakezero <sareahj.com@gmail.com>
2026-01-20 09:27:45 +0100

    fix: changed the redirect uri for prod to be coolify server

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -10,7 +10,7 @@ export const projectId = (import.meta as any).env?.VITE_REOWN_PROJECT_ID;
 export const discordAuthUrl =
 	(import.meta as any).env?.VITE_ENV === "development"
 		? "https://discord.com/oauth2/authorize" + "?client_id=" + (import.meta as any).env?.VITE_DISCORD_CLIENT_ID + "&redirect_uri=" + encodeURIComponent("http://localhost:5600/api/auth/discord/callback") + "&response_type=code" + "&scope=identify"
-		: "https://discord.com/oauth2/authorize" + "?client_id=" + (import.meta as any).env?.VITE_DISCORD_CLIENT_ID + "&redirect_uri=" + encodeURIComponent("https://nexura-app.onrender.com/api/auth/discord/callback") + "&response_type=code" + "&scope=identify";
+		: "https://discord.com/oauth2/authorize" + "?client_id=" + (import.meta as any).env?.VITE_DISCORD_CLIENT_ID + "&redirect_uri=" + encodeURIComponent("https://api-nexura.intuition.box/api/auth/discord/callback") + "&response_type=code" + "&scope=identify";
 
 export const VITE_X_CLIENT_ID = (import.meta as any).env?.VITE_X_CLIENT_ID;
 
