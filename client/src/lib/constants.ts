f5fcb2a72d85d6809fb2c9c91464733775bdb76c
cakezero <sareahj.com@gmail.com>
2026-01-28 15:14:31 +0100

    updated nexon addresses

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -16,43 +16,43 @@ export const VITE_X_CLIENT_ID = (import.meta as any).env?.VITE_X_CLIENT_ID;
 
 export const NEXONS: Record<number, { address: `0x${string}`, metadata: string }> = {
 	1: {
-		address: "0x40826ddd8eac2028719Faf5E2D4A506E2B27c90F",
+		address: "0x2ed45F846c812cCFdD2DfEcBA342fC717E3Fe093",
 		metadata: "ipfs://QmZ1pPLF9m6yGVQMETScwfAKm2ibpt4aZThuv1Dh8NaCCJ"
 	},
 	2: {
-		address: "0x622eCFc001aE94C9fA8F4d2dFAf8620F9a2F7A95",
+		address: "0xA762b1a7D236191E9656EE8d121171f501bA1CdC",
 		metadata: "ipfs://QmcPL2Wvcwn4qWpKgkyyywnYnDATUSa41cnGDsKkLTuBHW"
 	},
 	3: {
-		address: "0x3b246FCf564Bb6Cd307baBd19Bf319598cd064de",
+		address: "0xDB2f9756577ECfBcdf55C50Ac5BD71Cdb8c0B086",
 		metadata: "ipfs://QmPU499cvPM7SSvE6QY73sJxfRpog9NrsBd14yW11C5Tm9"
 	},
 	4: {
-		address: "0xF8dB53d37a246FD27E7746aAa7C8dDCf24ceF8F7",
+		address: "0xe1f25b80272BBed9E96E0E2e4Bd21a4cc0EbdBc0",
 		metadata: "ipfs://QmRedJkCoFPkcdYdoNJvDc1ydHRVZRJNrc4FKXkUqekm67"
 	},
 	5: {
-		address: "0xfddeecbf2cc6F3EEE73f6B81aF56730D482E4978",
+		address: "0x75343f041d3ac489a6E8757afD439d9D795b9dED",
 		metadata: "ipfs://QmScYrEhTjNzcERwUSXmX66cDCfD3yna8fdiUnLTk8DHEa"
 	},
 	6: {
-		address: "0xCBF7e2Fcef3E58631170712227283b418E0869e2",
+		address: "0x9AcF51aE75387A81E18F3174b9eA7f21C3634B61",
 		metadata: "ipfs://QmdBrFHCsyxnwEFSuCVTe7tb1dAxxSk9bjNBEodBAs6Cnm"
 	},
 	7: {
-		address: "0x76772a1Aef435358C255c3Fc2DF49E1c1E3f38B5",
+		address: "0x63AF87fE89348A0133c29BaEbD71055e0ee05c17",
 		metadata: "ipfs://QmbmiT1e4Sxf6AJhe3nEBqW6wSqA1UqQRn4Pg92ymiiGNS"
 	},
 	8: {
-		address: "0xffC95e1Ef2E20489AA6574C1F7cb6267D11D4C35",
+		address: "0x583548cD0b65595079Fa00CBe113b1230e48a92c",
 		metadata: "ipfs://Qma7hUkq8JmHLPhL12XAzMyYGovnsa34qoag9iXRtuCKnG"
 	},
 	9: {
-		address: "0x43Db0834e3C38F611cA8AA15277955Ff0A93C04C",
+		address: "0x7b0913C391fbc21cA7995535B00b69060fc590BE",
 		metadata: "ipfs://QmNrEQ53aobVuo9nRFJ1CBzzFdkC1QuUXkv7gSAP2RQbGr"
 	},
 	10: {
-		address: "0x4518442FbA0A15E4eA8dF274Cf8C59Aa39D5680C",
+		address: "0xF42ebE5136bCBD244c664c5A1eEC3012A293917F",
 		metadata: "ipfs://QmUdsL5f2v6E5iJRupdJBC7qzxHyvdV7qmMkCeTdg7k3As"
 	}
 };
