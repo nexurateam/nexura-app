f4f8be772395a0bb89cc9dd5a8137fd56fab4452
cakezero <sareahj.com@gmail.com>
2026-03-16 00:03:22 +0100

    changed name from wallet connect project id to reown project id

--- client/src/lib/wagmiConfig.ts
+++ client/src/lib/wagmiConfig.ts
@@ -2,7 +2,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import chain from "./chain";
 
 const walletConnectProjectId =
-  (import.meta as any).env?.VITE_WALLETCONNECT_PROJECT_ID || "PLACEHOLDER";
+  (import.meta as any).env?.VITE_REOWN_PROJECT_ID || "PLACEHOLDER";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Nexura",
